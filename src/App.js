fc9648f324a2c5224ce200177ca4731d711df078
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-08-16 20:39:31 +0530

    starting the project again from scratch.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-
-import './style/App.css';
-import Welcome from './components/Welcome';
-import Communication from './components/Communication';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* <Route path="/communication" element={<Communication />} /> */}
-        <Route path="/" element={<Welcome />} />
-
-      </Routes>
-    </Router>
+   <h1>hii
+  
+   </h1>
   );
 }
 
--- src/components/Greeting.js
+++
@@ -1,5 +0,0 @@
-function Greeting(name){
-    return(
-        <h1>hi, {name}!</h1>
-    )
-}
